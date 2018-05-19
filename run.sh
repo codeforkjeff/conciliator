f1b6f4770cd68b740fa7119e5d1c5c7c68fb90b6
codeforkjeff <jeff@codefork.com>
2018-05-19 14:34:33 -0700

    do error check for multiple jar files

--- run.sh
+++ run.sh
@@ -16,4 +16,11 @@
 
 JAR_PATH=`find target -type f -name "conciliator*.jar" -print`
 
+if [[ $JAR_PATH == *$'\n'* ]];
+then
+    echo ERROR: More than one jar file found in target/ directory.
+    echo Run \"mvn clean package\" then run this script again.
+    exit 1
+fi
+
 java -XX:+HeapDumpOnOutOfMemoryError -Xms256m -Xmx256m -Dlogging.level.com.codefork.refine=DEBUG -jar $JAR_PATH
