4cff32c31109c5d81e5ca9cce36e391607d3a40a
Jeff Chiu <jeff@codefork.com>
2023-03-21 09:01:05 -0700

    touch log file path otherwise docker creates it as a directory

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+touch conciliator.log
 docker run -d --restart always -p 127.0.0.1:8080:8080 -p 127.0.0.1:8081:8081 -p 127.0.0.1:8082:8082 -v "$(pwd)/conciliator.log:/opt/conciliator/conciliator.log" conciliator:latest
